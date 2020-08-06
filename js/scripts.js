224a0a7df87039879b56c026b991e6a53282b8de
Chris Ramer <chrisramer.97@gmail.com>
2020-08-06 15:05:30 -0700

    Add date form

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 $(document).ready(function () {
 
+	// Dropdown
 	$("form#beverageForm").submit(function (e) { 
 		e.preventDefault();
 		const beverage = $("#beverageOpts").val();
 		alert("You've chosen " + beverage + "!");
 	});
 
+	// Radio buttons
 	$("form#icecreamFlavor").submit(function (e) { 
 		e.preventDefault();
 		
 		const iceCream = $("input:radio[name=flavor]:checked").val();
 		alert("You've chosen " + iceCream + "!");
+	});
+
+	// Date
+	$("form#dateForm").submit(function (e) { 
+		e.preventDefault();
+		
+		const dob = $("#born").val();
+		alert("Your birthday is: " + dob + "!");
 	});
 
 });
