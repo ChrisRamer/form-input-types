a44fc1b6e0a1062d2c0726a3b36ac987743661d4
Chris Ramer <chrisramer.97@gmail.com>
2020-08-06 14:56:26 -0700

    Add radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,11 @@ $(document).ready(function () {
 		alert("You've chosen " + beverage + "!");
 	});
 
+	$("form#icecreamFlavor").submit(function (e) { 
+		e.preventDefault();
+		
+		const iceCream = $("input:radio[name=flavor]:checked").val();
+		alert("You've chosen " + iceCream + "!");
+	});
+
 });
